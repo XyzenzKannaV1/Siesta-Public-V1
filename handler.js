317daf54fb58b27632d47d88fe008f2fba9ba9d7
Kanna <112399198+XyzenzKannaV1@users.noreply.github.com>
2022-10-18 22:45:56 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -133,6 +133,7 @@ module.exports = async (client, m) => {
             if (setting.pluginDisable.includes(name)) return client.reply(m.chat, Func.texted('bold', `ðŸš© Plugin disabled by Owner.`), m)
             if (!m.isGroup && !['owner', 'create_bot'].includes(name) && chats && !isPrem && !users.banned && new Date() * 1 - chats.lastchat < global.timer) continue
             if (!m.isGroup && !['owner', 'create_bot'].includes(name) && chats && !isPrem && !users.banned && setting.groupmode) return client.sendMessageModify(m.chat, `ðŸš© Using bot in private chat only for premium user, upgrade to premium plan only Rp. 5,000,- to get 1K limits.\n\nIf you want to buy contact *${prefixes[0]}owner*`, m, {
+               title : 'Siesta Beta Test',
                largeThumb: true,
                thumbnail: await Func.fetchBuffer('https://telegra.ph/file/0b32e0a0bb3b81fef9838.jpg'),
                url: 'https://chat.whatsapp.com/KbgGdT1cV3rD5JzJDZryis'
@@ -211,7 +212,7 @@ module.exports = async (client, m) => {
             if (setting.pluginDisable.includes(name)) continue
             if (!m.isGroup && chats && !isPrem && !users.banned && new Date() * 1 - chats.lastchat < global.timer) continue
             if (!m.isGroup && chats && !isPrem && !users.banned && setting.groupmode) return client.sendMessageModify(m.chat, `ðŸš© Using bot in private chat only for premium user, upgrade to premium plan only Rp. 5,000,- to get 1K limits.\n\nIf you want to buy contact *${prefixes[0]}owner*`, m, {
-               title: 'Siesta Beta Test'
+               title: 'Siesta Beta Test',
                largeThumb: true,
                thumbnail: await Func.fetchBuffer(''https://telegra.ph/file/1d91c4652a272a848aaa9.jpg"),
                url: 'https://chat.whatsapp.com/KbgGdT1cV3rD5JzJDZryis'
